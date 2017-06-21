d6d37dd8f09a38691a762b0b98cacb4505da545c
mitkat <miteneva.kate@gmail.com>
2017-06-22 00:35:16 +0300

    сделала работу шапки без js, установила полифил

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task("style", function() {
         "last 2 versions"
       ]}),
       mqpacker({
-        sort: true
+        sort: false
       })
     ]))
     .pipe(gulp.dest("build/css"))
